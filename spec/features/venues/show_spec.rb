fe08fb72e3d73cab8ad7f70d72857bf7be4469a0
Ely Hess <ely.hess@me.com>
2020-12-02 15:51:58 -0700

    Add delete_venue test

--- spec/features/venues/show_spec.rb
+++ spec/features/venues/show_spec.rb
@@ -13,5 +13,17 @@ describe 'As a visitor' do
       expect(page).to have_content(red_rocks.outdoor)
       expect(page).to_not have_content(blue_rocks.name)
     end
+
+    it 'allows me to delete a venue' do
+      red_rocks = Venue.create(name: "red rocks", capacity: 11000, outdoor: true)
+      blue_rocks = Venue.create(name: "blue rocks", capacity: 1100, outdoor: false)
+
+      visit "/venues/#{red_rocks.id}"
+      click_link "Remove Band"
+
+      expect(current_path).to eq("/venues")
+      expect(page).to_not have_content(red_rocks.name)
+      expect(page).to have_content(blue_rocks.name)
+    end
   end
 end
