0e70d2e543321a21786d244388ee7bd905403a31
Ely Hess <ely.hess@me.com>
2020-12-02 14:43:15 -0700

    Add story 3, venues

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -6,4 +6,15 @@ class VenuesController < ApplicationController
   def show
     @venue = Venue.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    Venue.create(name: params[:name],
+                 capacity: params[:capacity],
+                 outdoor: params[:outdoor])
+    redirect_to "/venues"
+  end
+  
 end
