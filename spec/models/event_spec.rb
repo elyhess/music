1d9ca95cff6ee0c96e4899152c0da6616b0df6f0
awosborn <alanturing@example.coclear>
2020-12-03 16:42:54 -0700

    Add Event model test

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Event do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :venues}
+  end
+
+  describe 'instance methods' do
+  end
+end
