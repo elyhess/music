4de63e4b5759a863c50fae9761b04dd668484572
Ely Hess <ely.hess@me.com>
2020-12-04 11:21:46 -0700

    add crud routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
   # Events
   get '/events', to: 'events#index'
   get '/events/:id', to: 'events#show'
+  get '/events/:id/edit', to: 'events#edit'
+  patch '/events/:id', to: 'events#update'
+  delete '/events/:id', to: 'events#destroy'
   
   # Members
   get '/members', to: 'members#index'
@@ -29,6 +32,8 @@ Rails.application.routes.draw do
   #BandMembers
   get '/bands/:id/members', to: 'band_members#index'
 
-  #VenueEvents
+  # VenueEvents
   get '/venues/:id/events', to: 'venue_events#index'
+  get '/venues/:id/events/new', to: 'venue_events#new'
+  post '/venues/:id/events', to: 'venue_events#create'
 end
