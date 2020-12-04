172742f4201415e474e48d75496ef63bca6b3626
Ely Hess <ely.hess@me.com>
2020-12-04 11:23:01 -0700

    Add order_by & date_added methods

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,12 @@ class Venue < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :capacity
   has_many :events, dependent: :destroy
+
+  def self.order_by_most_recent
+    Venue.order(outdoor: :DESC, created_at: :DESC)
+  end
+
+  def date_added
+    created_at.strftime("%m/%d/%Y %H:%M")
+  end
 end
