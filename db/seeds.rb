c98698c0bd0e77874ef222ec921fe2e94cd25eb2
Ely Hess <ely.hess@me.com>
2020-12-04 11:22:04 -0700

    Add factorybot & FFaker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# rails db:purge db:migrate db:seed (to reset autoincremented ids)
+
+Venue.destroy_all
+Event.destroy_all
+
+Band.destroy_all
+Member.destroy_all
+
+FactoryBot.create_list(:venue, 20)
+
+Venue.all.each do |venue|
+  10.times do
+    venue.events.create(name: FFaker::Company.bs)
+  end
+end
+
+FactoryBot.create_list(:band, 20)
+
+Band.all.each do |band|
+  10.times do
+    band.members.create(name: FFaker::Company.bs)
+  end
+end
