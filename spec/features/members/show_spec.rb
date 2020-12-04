8832c342228567a583dfcf03bd712ca72e317f85
awosborn <alanturing@example.coclear>
2020-12-04 16:19:56 -0700

    Add Delete function test

--- spec/features/members/show_spec.rb
+++ spec/features/members/show_spec.rb
@@ -11,5 +11,21 @@ describe 'As a visitor' do
 
       expect(page).to have_content(member_1.name)
     end
+
+    it 'allows me to delete a member' do
+       lewis_del_mar = Band.create(name: "Lewis Del Mar", booked: true)
+
+      maximus = lewis_del_mar.members.create(name: "Max", instrument:"vocals")
+      daniel = lewis_del_mar.members.create(name: "Daniel", instrument:"computer")
+
+      visit "/members/#{maximus.id}"
+
+      click_link 'Delete Member'
+
+      expect(current_path).to eq('/members')
+
+      expect(page).to_not have_content(maximus.name)
+      expect(page).to have_content(daniel.name)
+    end
   end
 end
