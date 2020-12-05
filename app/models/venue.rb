63dec0ce924521f041784917c65756881f8022bc
Ely Hess <ely.hess@me.com>
2020-12-05 11:01:20 -0700

    Add min_capacity method

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,10 +4,15 @@ class Venue < ApplicationRecord
   has_many :events, dependent: :destroy
 
   def self.order_by_most_recent
-    Venue.order(outdoor: :DESC, created_at: :DESC)
+    order(outdoor: :DESC, created_at: :DESC)
+  end
+
+  def self.min_capacity(amount)
+    where('capacity > ?', amount)
   end
 
   def date_added
     created_at.strftime("%m/%d/%Y %H:%M")
   end
+  
 end
