454d65eb8364d70d5a47682a10612197d4fadd4f
Ely Hess <ely.hess@me.com>
2020-12-04 17:13:59 -0700

    Add filter form feature/test

--- spec/features/venues/index_spec.rb
+++ spec/features/venues/index_spec.rb
@@ -17,5 +17,18 @@ describe 'As a visitor' do
       expect(page).to have_content("Capacity: #{blue_rocks.capacity}")
       expect(page).to have_link("New Venue")
     end
+    it 'Has a form field that allows search results filtered by capacity'
+      red_rocks = Venue.create!(name: "red rocks", capacity: 11000, outdoor: true)
+      blue_rocks = Venue.create!(name: "blue rocks", capacity: 1100, outdoor: false)
+
+      visit '/venues'
+
+      fill_in 'Return results with capacity greater than: ', with: 1101
+      
+      click_on 'Go'
+
+      expect(page).to have_content("Blue Rocks")
+      expect(page).to_nothave_content("Red Rocks")
+    end
   end
 end
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -22,6 +22,17 @@ RSpec.describe Venue do
 
       expect(Venue.order_by_most_recent).to eq([teal_rocks, yellow_rocks, red_rocks, black_rocks, orange_rocks, blue_rocks])
     end
+
+    it '.limit_results_below' do
+      red_rocks = Venue.create!(name: "red rocks", capacity: 11000, outdoor: true, created_at: 'Thu, 01 Dec 2011 19:23:11 UTC +00:00')
+      blue_rocks = Venue.create!(name: "blue rocks", capacity: 11, outdoor: false, created_at: 'Thu, 01 Dec 2012 19:23:11 UTC +00:00')
+      yellow_rocks= Venue.create!(name: "yellow rocks", capacity: 1123, outdoor: true, created_at: 'Thu, 01 Dec 2013 19:23:11 UTC +00:00')
+      orange_rocks = Venue.create!(name: "orange rocks", capacity: 2332, outdoor: false, created_at: 'Thu, 01 Dec 2014 19:23:11 UTC +00:00')
+      teal_rocks= Venue.create!(name: "teal rocks", capacity: 3234, outdoor: true, created_at: 'Thu, 01 Dec 2015 19:23:11 UTC +00:00')
+      black_rocks = Venue.create!(name: "black rocks", capacity: 1144, outdoor: false, created_at: 'Thu, 01 Dec 2016 19:23:11 UTC +00:00')
+
+      expect(Venue.limit_results_below(2000).to eq([blue_rocks, yellow_rocks, black_rocks])
+    end
   end
 
   describe 'instance methods' do
