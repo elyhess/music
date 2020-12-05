95b26f85a1f3b187c73d4345165a5c6b4f62eade
Ely Hess <ely.hess@me.com>
2020-12-05 11:00:33 -0700

    Add min_capacity test

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Venue do
       expect(Venue.order_by_most_recent).to eq([teal_rocks, yellow_rocks, red_rocks, black_rocks, orange_rocks, blue_rocks])
     end
 
-    it '.limit_results_below' do
+    it '.min_capacity' do
       red_rocks = Venue.create!(name: "red rocks", capacity: 11000, outdoor: true, created_at: 'Thu, 01 Dec 2011 19:23:11 UTC +00:00')
       blue_rocks = Venue.create!(name: "blue rocks", capacity: 11, outdoor: false, created_at: 'Thu, 01 Dec 2012 19:23:11 UTC +00:00')
       yellow_rocks= Venue.create!(name: "yellow rocks", capacity: 1123, outdoor: true, created_at: 'Thu, 01 Dec 2013 19:23:11 UTC +00:00')
@@ -31,7 +31,7 @@ RSpec.describe Venue do
       teal_rocks= Venue.create!(name: "teal rocks", capacity: 3234, outdoor: true, created_at: 'Thu, 01 Dec 2015 19:23:11 UTC +00:00')
       black_rocks = Venue.create!(name: "black rocks", capacity: 1144, outdoor: false, created_at: 'Thu, 01 Dec 2016 19:23:11 UTC +00:00')
 
-      expect(Venue.limit_results_below(2000).to eq([blue_rocks, yellow_rocks, black_rocks])
+      expect(Venue.min_capacity(2000)).to eq([red_rocks, orange_rocks, teal_rocks])
     end
   end
 
