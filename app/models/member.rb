ae94e9626dbf0e870977445f44813861d68c475a
awosborn <alanturing@example.coclear>
2020-12-06 11:51:32 -0700

    Add feature test member_count

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,9 @@ class Member < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :instrument
   belongs_to :band
+
+  def self.member_count
+    count
+  end
+
 end
--- spec/features/band_members/index_spec.rb
+++ spec/features/band_members/index_spec.rb
@@ -4,16 +4,18 @@ describe 'As a visitor' do
   describe 'when I visit band/band_id/members/:' do
     it 'Then I see the members that is associated with the band and attributes' do
 
-      lewis_del_mar = Band.create(name: "Lewis Del Mar", booked: true)
-      member_1 = lewis_del_mar.members.create(name: "person1", instrument:"vocals")
-      member_2 = lewis_del_mar.members.create(name: "person2", instrument:"computer")
+      lewis_del_mar = Band.create!(name: "Lewis Del Mar", booked: true)
+      maximus = lewis_del_mar.members.create!(name: "Max", instrument:"vocals")
+      daniel = lewis_del_mar.members.create!(name: "Daniel", instrument:"computer")
 
       visit "/bands/#{lewis_del_mar.id}/members"
 
-      expect(page).to have_content(member_1.name)
-      expect(page).to have_content(member_2.name)
-      expect(page).to have_content(member_1.instrument)
-      expect(page).to have_content(member_2.instrument)
+      expect(page).to have_content(maximus.name)
+      expect(page).to have_content(daniel.name)
+      expect(page).to have_content(maximus.instrument)
+      expect(page).to have_content(daniel.instrument)
+      expect(page).to have_content("Member Count#{lewis_del_mar.members.member_count}")
+      
   end
 end
 end
