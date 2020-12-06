b96ca142283f061c4a9cfc7bb273f67c303450c3
Ely Hess <ely.hess@me.com>
2020-12-06 11:56:17 -0700

    Add total_amount view test

--- spec/features/venue_events/index_spec.rb
+++ spec/features/venue_events/index_spec.rb
@@ -15,6 +15,17 @@ describe 'As a visitor' do
       expect(page).to have_content(summer_splash.name)
     end
 
+    it 'Shows me the total amount of' do
+      venue_1 = Venue.create!(name: 'Great Stage Park', capacity: 90000, outdoor: true)
+
+      summer_jam = venue_1.events.create!(name: "Summer Jam")
+      summer_splash = venue_1.events.create!(name: "Summer Splash")
+
+      visit "/venues/#{venue_1.id}/events/"
+
+      expect(page).to have_content("Total: #{venue_1.events.event_count}")
+    end
+
     it "I can click on the 'Sort alphabetically' to sort events" do
       venue_1 = Venue.create!(name: 'Great Stage Park', capacity: 90000, outdoor: true)
 
