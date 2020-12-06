a8aa9e5f91f8f44151dc09b7294e3720eda03e03
awosborn <alanturing@example.coclear>
2020-12-06 13:24:35 -0700

    Add feature test story 17, bands

--- spec/features/bands/index_spec.rb
+++ spec/features/bands/index_spec.rb
@@ -13,5 +13,25 @@ describe 'As a visitor' do
       expect(page).to have_content(lewis_del_mar.date_added)
       expect(mac_demarco.name).to appear_before(lewis_del_mar.name)
     end
+
+    it "I see a link to sort by member count, when clicked orders bands" do
+      lewis_del_mar = Band.create(name: "Lewis Del Mar", booked: true, created_at: 'Thu, 03 Dec 2020 19:24:11 UTC +00:00')
+      maximus = lewis_del_mar.members.create!(name: "Max", instrument:"vocals")
+      daniel = lewis_del_mar.members.create!(name: "Daniel", instrument:"computer")
+
+      cats_in_bandanas = Band.create(name: "Cats in Bandans", booked: true, created_at: 'Thu, 05 Dec 2020 19:24:11 UTC +00:00')
+      kitteh = cats_in_bandanas.members.create!(name: "Kitteh in the Hat", instrument:"Meows")
+
+      visit '/bands'
+
+      expect(page).to have_link("Sort by band size")
+
+      click_on "Sort by band size"
+
+      expect(current_path).to eq('/bands')
+      expect(page).to have_content("#{lewis_del_mar.name} - Members: 2")
+      expect(page).to have_content("#{cats_in_bandanas.name} - Members: 1")
+      expect(lewis_del_mar.name).to appear_before(cats_in_bandanas.name)
+    end
   end
 end
