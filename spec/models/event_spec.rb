cea6c4dc3640b9375d8d21e08ae9563c63aad99b
Ely Hess <ely.hess@me.com>
2020-12-06 09:51:21 -0700

    add event_count test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,6 +8,26 @@ RSpec.describe Event do
   describe 'relationships' do
     it {should belong_to :venue}
   end
+  
+  describe 'class methods' do
+    it '.event_count' do
+      red_rocks = Venue.create!(name: "red rocks", capacity: 11000, outdoor: true)
+      red_rocks.events.create!(name: "event1")
+      red_rocks.events.create!(name: "event2")
+      red_rocks.events.create!(name: "event3")
+
+      expect(Event.event_count).to eq(3)
+    end
+
+    it '.sort_alphabetically' do
+      red_rocks = Venue.create!(name: "red rocks", capacity: 11000, outdoor: true)
+      event_1 = red_rocks.events.create!(name: "X")
+      event_2 = red_rocks.events.create!(name: "D")
+      event_3 = red_rocks.events.create!(name: "A")
+
+      expect(Event.sort_alphabetically).to eq([event_3, event_2, event_1])
+    end
+  end
 
   describe 'instance methods' do
   end
