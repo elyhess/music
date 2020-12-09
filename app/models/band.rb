02d2edea25fb6ae7e60c05d524e347cf8de86cd7
Ely Hess <ely.hess@me.com>
2020-12-09 14:32:39 -0700

    remove date_added

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,10 +2,6 @@ class Band < ApplicationRecord
   validates_presence_of :name
   has_many :members, dependent: :destroy
 
-  def date_added
-    created_at.strftime("%m/%d/%Y %H:%M")
-  end
-
   def self.order_by_most_recent
     order(booked: :DESC, created_at: :DESC)
   end
